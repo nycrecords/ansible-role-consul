289938a8c7ebee49b62cd38aa1420252b8e14b5a
Gary Zhou <gzhou@records.nyc.gov>
2020-07-23 14:28:26 -0400

    Modify tests and fix dnsmasq service task

--- molecule/resources/tests/test_default.py
+++ molecule/resources/tests/test_default.py
@@ -7,12 +7,13 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ["MOLECULE_INVENTORY_FILE"]
 ).get_hosts("all")
 
-def test_consul_is_installed(host):
-    consul = host.file("/usr/local/bin/consul")
 
-    assert consul.is_symlink
-    with host.sudo():
-        assert "/opt/consul/consul" == consul.linked_to
+def test_consul_is_running_and_enabled(host):
+    consul = host.service("consul")
+
+    assert consul.is_running
+    assert consul.is_enabled
+
 
 def test_dnsmasq_is_installed(host):
     dnsmasq = host.file("/usr/sbin/dnsmasq")
